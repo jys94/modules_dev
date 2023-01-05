de84bd0f06816c1bb5de2300796e9012ae53a174
jys94 <115811321+jys94@users.noreply.github.com>
2023-01-05 10:21:38 +0900

    Update rule.tf

--- modules/alb-rule/rule.tf
+++ modules/alb-rule/rule.tf
@@ -2,22 +2,29 @@ variable "LISTENER_ARN" {}
 variable "PRIORITY" {}
 variable "TARGET_GROUP_ARN" {}
 variable "CONDITION_FIELD" {}
-
-variable "CONDITION_VALUES" {
-  type = list(string)
-}
+variable "CONDITION_VALUES" { type = list(string) }
 
 resource "aws_lb_listener_rule" "alb_rule" {
-  listener_arn = "${var.LISTENER_ARN}"
-  priority     = "${var.PRIORITY}"
+  listener_arn = var.LISTENER_ARN
+  priority     = var.PRIORITY
 
   action {
     type             = "forward"
-    target_group_arn = "${var.TARGET_GROUP_ARN}"
+    target_group_arn = var.TARGET_GROUP_ARN
   }
 
   condition {
-    field  = "${var.CONDITION_FIELD}"
-    values = ["${var.CONDITION_VALUES}"]
+    dynamic host_header {
+      for_each = var.CONDITION_FIELD == "host-header" ? [1] : []
+      content {
+        values = var.CONDITION_VALUES
+      }
+    }
+    dynamic path_pattern {
+      for_each = var.CONDITION_FIELD == "path-pattern" ? [1] : []
+      content {
+        values = var.CONDITION_VALUES
+      }
+    }
   }
 }
